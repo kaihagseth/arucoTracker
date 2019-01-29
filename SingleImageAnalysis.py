69afeba547a5fda36411adf670b00e1abf00aace
Vegarfjo <vegarfjo@stud.ntnu.no>
2019-01-29 16:01:24 +0100

    Correcting branches in sourcetree

--- SingleImageAnalysis.py
+++ SingleImageAnalysis.py
@@ -1,5 +1,4 @@
 import numpy as np
-import cv2
 import math
 import exceptions as exc
 
@@ -22,20 +21,6 @@ class SingleImageAnalysis():
         '''
         Estimate the model pose from a single image.
         :param imagePoints: Image coordinates of the point location, given in number of pixels. Order is not essential. Given as 4x2 matrix. If
-<<<<<<< HEAD
-        point is not found, it's x's and y's are set to -1. Image origo is top left, +y is downwards.
-        :param distKoeff: Distortion coefficients of the lense. Given as [coeff_x, coeff_y]
-        :param intrCamMtrx: The intrinsic camera matrix. Given as: [[fx, 0, cx],[0, fy, cy],[0,0,1]]
-        :param modellParam: The location of on-model bullets, given in model coordinates. Given as a 4x3 matrix. If not
-        specified, default parameters is used. Default: [[1,0,0],[0,1,0],[0,0,1], [0,0,0]]
-        :return: Extrinsic matrix describing pose and position of camera. Given as 3x4 matrix.
-        '''
-        if intrCamMtrx is None:
-            intrCamMtrx = np.matrix([[1,0,0],[0,1,0],[0,0,1], [0,0,0]])
-
-    def findBallPoints(self):
-        pass
-=======
         point is not found, its x's and y's are set to -1. Image origo is top left, +y is downwards.
         :param intrCamMtrx: The intrinsic camera matrix. Given as: [[fx, 0, cx],[0, fy, cy],[0,0,1]]
         :param modelParam: The location of on-model bullets, given in homogenus model coordinates. Given as a 4x3 matrix. If not
@@ -135,10 +120,3 @@ class SingleImageAnalysis():
             x = x + dx
 
         return x
-
-
-
-
-
-
->>>>>>> PoseEstimation
