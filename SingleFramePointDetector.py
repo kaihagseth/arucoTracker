0b1ad334fd6409500b620c8d760c2ceb3abebf48
Kai Hagseth <kai.hagseth@ntnu.no>
2019-02-06 15:46:29 +0100

    Refactored the Single Point Detector class to save their hsv-values in fields, and added a calibration function. Removed the findBallPoints-function from camera class since this is now found in singleFramePointDetector.

--- SingleFramePointDetector.py
+++ SingleFramePointDetector.py
@@ -4,15 +4,84 @@ import imutils
 import numpy as np
 
 
+def callback(x):
+    pass
+
 class SingleFramePointDetector:
     '''
     Detect points in a single frame.
     '''
     def __init__(self):
+        self.lower_hue = 0
+        self.lower_saturation = 0
+        self.lower_value = 0
+        self.upper_hue = 180
+        self.upper_saturation = 255
+        self.upper_value = 255
+        self.NUMBER_OF_POINTS = 3
         pass
 
-    @staticmethod
-    def findBallPoints(frame, lower_bounds, upper_bounds):
+    def setHSVValues(self, lower_values, upper_values):
+        """
+        :param lower_values: List of lower bound hsv values.
+        :param upper_values:  List of upper bound hsv values.
+        :return: None
+        """
+        self.lower_hue = lower_values(0)
+        self.lower_saturation = lower_values(1)
+        self.lower_value = lower_values(2)
+        self.upper_hue = upper_values(0)
+        self.upper_saturation = upper_values(1)
+        self.upper_value = upper_values(2)
+
+
+    def calibrate(self, cap):
+        """
+        :param cap: Videocapture-stream from camera to be calibrated
+        :return: None
+        """
+        cv2.namedWindow('image')
+        cv2.createTrackbar('lowH', 'image', self.lower_hue, 179, callback)
+        cv2.createTrackbar('highH', 'image', self.upper_hue, 179, callback)
+
+        cv2.createTrackbar('lowS', 'image', self.lower_saturation, 255, callback)
+        cv2.createTrackbar('highS', 'image', self.upper_saturation, 255, callback)
+
+        cv2.createTrackbar('lowV', 'image', self.lower_value, 255, callback)
+        cv2.createTrackbar('highV', 'image', self.upper_value, 255, callback)
+
+        while (True):
+            # grab the frame
+            ret, frame = cap.read()
+
+            # get trackbar positions
+            self.lower_hue = cv2.getTrackbarPos('lowH', 'image')
+            self.upper_hue = cv2.getTrackbarPos('highH', 'image')
+            self.lower_saturation = cv2.getTrackbarPos('lowS', 'image')
+            self.upper_saturation = cv2.getTrackbarPos('highS', 'image')
+            self.lower_value = cv2.getTrackbarPos('lowV', 'image')
+            self.upper_value = cv2.getTrackbarPos('highV', 'image')
+
+            if self.lower_hue < self.upper_hue:
+                hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+                lower_hsv = np.array([self.lower_hue, self.lower_saturation, self.lower_value])
+                higher_hsv = np.array([self.upper_hue, self.upper_saturation, self.upper_value])
+            else:
+                hsv = cv2.cvtColor(frame, cv2.COLOR_RGB2HSV)
+                lower_hsv = np.array([self.lower_hue - 90, self.lower_saturation, self.lower_value])
+                higher_hsv = np.array([self.higher_hue + 90, self.upper_saturation, self.upper_value])
+            mask = cv2.inRange(hsv, lower_hsv, higher_hsv)
+
+            image = cv2.bitwise_and(frame, frame, mask=mask)
+
+            # show thresholded image
+            cv2.imshow('image', image)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        cv2.destroyAllWindows()
+
+
+    def findBallPoints(self, frame):
         """" Finds center points and radii of the largest circles in the image.
         :param frame: BGR-image to analyze
         :param lower_bounds: hsv lower bounds np array ex(170, 100, 100) for red
@@ -21,17 +90,17 @@ class SingleFramePointDetector:
         4 largest circles in the frame. In cases where less circles are detected,
         remaining rows will returns with -1 """
         # blur image to remove hf-noise
-        NUMBER_OF_POINTS = 3
         blurred = cv2.GaussianBlur(frame, (11, 11), 0)
-        lower_bounds = np.array(lower_bounds)
-        upper_bounds = np.array(upper_bounds)
-        # Hack to bypass inRange's limitation of looping around 0 in the H-channel in HSV.
-        if lower_bounds[0] < upper_bounds[0]:
-            hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+        if self.lower_hue < self.upper_hue:
+            hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+            lower_bounds = np.array([self.lower_hue, self.lower_saturation, self.lower_value])
+            upper_bounds = np.array([self.upper_hue, self.upper_saturation, self.upper_value])
         else:
-            lower_bounds[0] -= 90
-            upper_bounds[0] += 90
-            hsv = cv2.cvtColor(blurred, cv2.COLOR_RGB2HSV)
+            hsv = cv2.cvtColor(frame, cv2.COLOR_RGB2HSV)
+            lower_bounds = np.array([self.lower_hue - 90, self.lower_saturation, self.lower_value])
+            upper_bounds = np.array([self.higher_hue + 90, self.upper_saturation, self.upper_value])
+
+
 
         # construct a mask for the color, then perform
         # a series of dilations and erosions to remove any small
@@ -40,13 +109,14 @@ class SingleFramePointDetector:
         mask = cv2.erode(mask, None, iterations=2)
         mask = cv2.dilate(mask, None, iterations=2)
 
+
         # find contours in the mask and initialize the current
         # (x, y) center of the ball
         contours = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         contours = imutils.grab_contours(contours)
 
         # only proceed if at least one contour was found
-        enclosed_circles = np.ones((NUMBER_OF_POINTS, 3)) * -1
+        enclosed_circles = np.ones((self.NUMBER_OF_POINTS, 3)) * -1
         if len(contours) == 0:
             return enclosed_circles
         all_circles = []
@@ -64,8 +134,8 @@ class SingleFramePointDetector:
         # centroids
         if len(all_circles) == 0:
             return enclosed_circles
-        elif len(all_circles) > NUMBER_OF_POINTS:
-            largest_circles = nlargest(NUMBER_OF_POINTS, all_circles, key=cv2.contourArea)
+        elif len(all_circles) > self.NUMBER_OF_POINTS:
+            largest_circles = nlargest(self.NUMBER_OF_POINTS, all_circles, key=cv2.contourArea)
         else:
             largest_circles = [0]*len(all_circles)
             for (num, circle) in enumerate(all_circles):
